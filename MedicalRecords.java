8d92398bf7b7e4ee358f0bd4954e75e2292074e7
Anurag Kumar <ark88ark2121@gmail.com>
2021-07-31 13:12:37 +0530

    medRec

--- MedicalRecords.java
+++ MedicalRecords.java
@@ -25,5 +25,68 @@ Instance Methods-
     private boolean admitted;
     private LabTests[] fineLabTest;
     private LabTests[] needDoctorAttentionTest;
-    
+
+	public String getChronicDisease() {
+		return chronicDisease;
+	}
+
+	public void setChronicDisease(String chronicDisease) {
+		this.chronicDisease = chronicDisease;
+	}
+
+	public boolean isHyperTension() {
+		return hyperTension;
+	}
+
+	public void setHyperTension(boolean hyperTension) {
+		this.hyperTension = hyperTension;
+	}
+
+	public String getPreviousDisease() {
+		return previousDisease;
+	}
+
+	public void setPreviousDisease(String previousDisease) {
+		this.previousDisease = previousDisease;
+	}
+
+	public Hospital getHospital() {
+		return hospital;
+	}
+
+	public void setHospital(Hospital hospital) {
+		this.hospital = hospital;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+
+	public boolean isAdmitted() {
+		return admitted;
+	}
+
+	public void setAdmitted(boolean admitted) {
+		this.admitted = admitted;
+	}
+
+	public LabTests[] getFineLabTest() {
+		return fineLabTest;
+	}
+
+	public void setFineLabTest(LabTests[] fineLabTest) {
+		this.fineLabTest = fineLabTest;
+	}
+
+	public LabTests[] getNeedDoctorAttentionTest() {
+		return needDoctorAttentionTest;
+	}
+
+	public void setNeedDoctorAttentionTest(LabTests[] needDoctorAttentionTest) {
+		this.needDoctorAttentionTest = needDoctorAttentionTest;
+	}
 }
