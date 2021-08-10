5f5a179b9770a0b803842d87f49d74cf3b66cfb5
durga207 <87663826+durga207@users.noreply.github.com>
2021-08-10 22:36:01 +0530

    Update Bill.java

--- Bill.java
+++ Bill.java
@@ -1,59 +1,72 @@
 import java.util.Scanner;
 
 
+import java.util.Scanner;
+
 public class Bill {
     private MedicalRecords medRecord;
 
-    public Bill(MedicalRecords medRecords){
+    public Bill(MedicalRecords medRecords) {
         this.medRecord = medRecords;
     }
 
-    public void generateBill(){
-        double docFee = medRecord.getDoctor().getFees(),bill;
-        double roomFee = 0;
-        if (medRecord.isBolAdmitted()) {
-            roomFee += medRecord.getHospital().getCostPerRoom();
-        }
-        double labTestsCharge=0;
+    public void generateBill() {
+        double docFee = medRecord.getDoctor().getFees(), bill;
+        double roomFee = medRecord.getHospital().getCostPerRoom();
+        double labTestsCharge = 0;
         LabTests[] tests = medRecord.getFineLabTest();
         LabTests[] tests2 = medRecord.getNeedDoctorAttentionTest();
 
-        for(int i=0 ;i <medRecord.getFineLabTest().length;i++){
-            if(tests[i]==null){
+        for (int i = 0; i < medRecord.getFineLabTest().length; i++) {
+            if (tests[i] == null) {
                 break;
             }
             labTestsCharge += tests[i].getCost();
         }
 
-        for(int i=0; i<medRecord.getNeedDoctorAttentionTest().length; i++){
-            if(tests2[i]==null){
+        for (int i = 0; i < medRecord.getNeedDoctorAttentionTest().length; i++) {
+            if (tests2[i] == null) {
                 break;
             }
             labTestsCharge += tests2[i].getCost();
         }
 
-        bill = docFee + roomFee*medRecord.getDoctor().recomStayInHospital() + labTestsCharge;
+        int numberOfDays;
+        if (medRecord.isBolAdmitted()) {
+            numberOfDays = medRecord.getDoctor().recomStayInHospital();
+        } else {
+            numberOfDays = 0;
+        }
+        bill = docFee + roomFee * numberOfDays + labTestsCharge;
 
-        bill += (18/100)*bill; 
+        bill += (bill * 18) / 100;
 
-        System.out.println("---------------Bill Explained---------------\n"+
-        "Doctors fee - " + docFee + "\nRoom charges(Number of days admitted * charges per day) - " +
-        roomFee*medRecord.getDoctor().recomStayInHospital() + "\nLab Tests Charges - " + labTestsCharge +
-        "\nTotal Bill (Hospital Charges + 18 % GST) - " + bill
+        System.out.println("---------------Bill Explained---------------\n" +
+                "Doctors fee - " + docFee +
+                "\nNumber of days admitted - " + numberOfDays
         );
-        
+        if (medRecord.isBolAdmitted()) {
+            System.out.println("Room fee per day - " + roomFee +
+                    "\nRoom charges(Number of days admitted * charges per day) - " + (roomFee * numberOfDays)
+            );
+        }
+        System.out.println("Lab Tests Charges - " + labTestsCharge +
+                "\nTotal Bill (Hospital Charges + 18 % GST) - " + bill
+        );
+
+
     }
 
-    public boolean isBillPaid(){
+    public boolean isBillPaid() {
         return true;
     }
 
-    public String billPayMode(){
+    public String billPayMode() {
         System.out.println("\n\nChoose one of the following payment mode: ");
         System.out.println("\n 1. Card \n 2. NetBanking \n 3. Cash \n 4. Cheque \n \n Enter the number here : ");
         Scanner scan1 = new Scanner(System.in);
         int input = scan1.nextInt();
-        switch(input){
+        switch (input) {
             case 1:
                 return "Card";
             case 2:
@@ -68,3 +81,4 @@ public class Bill {
 
     }
 }
+
