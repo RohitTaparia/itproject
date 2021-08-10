5530af8480bbc195696f8e1629b3cfde34ed2f35
durga207 <87663826+durga207@users.noreply.github.com>
2021-08-10 18:48:55 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,28 +9,33 @@ public class Main {
         // information about the selected hospital.
         // Displays the depts in that hospital and asks to select one of those.
         PatientMapping p1 = new PatientMapping();
-        String HspFile = "itproject/data/Hospitals.txt";
-        String DctrFile ="itproject/data/doctor.txt";
-        MedicalRecords medRec = new MedicalRecords("ABC",true , "DEF"); // Medical Record Defined
+        String HspFile = "C:\\Users\\Praveen Raj HL\\Desktop/ITproject.txt";
+        String DctrFile = "C:\\Users\\Praveen Raj HL\\Desktop/doctor.txt";
+        MedicalRecords medRec = new MedicalRecords("ABC", true, "DEF"); // Medical Record Defined
 
         Location l1 = new Location(28.6052154684654, 77.2155121525);
         p1.recommend(l1, 10, HspFile);
+//        for (int i = 0; i < p1.getNearest().length; i++) {
+//            System.out.println(i + "--------------------------------");
+//            System.out.println(p1.getNearest()[i]);
+//
+//        }
         Scanner myreader = new Scanner(System.in);
         System.out.println("Enter the Hospital number you chose: ");
         int indexHsp = myreader.nextInt();
-        medRec.setHospital(p1.selectHsp(indexHsp - 1));  // putting hospital in medical Records and also using selectHsp method
+        medRec.setHospital(p1.selectHsp((indexHsp - 1)));  // putting hospital in medical Records and also using selectHsp method
         System.out.println("--------------------------------------------------------------------");
-        p1.getHspArray()[indexHsp - 1].printDepts(p1, DctrFile,medRec);
+        p1.getNearest()[indexHsp - 1].printDepts(p1, DctrFile, medRec);
 
         //Room
-        Room r1 = new Room(p1.getHspArray()[indexHsp - 1].getID(), p1.getHspArray()[indexHsp - 1].getName());
-        r1.generateRoom(HspFile);l̥
+        Room r1 = new Room(p1.getNearest()[indexHsp - 1].getID(), p1.getNearest()[indexHsp - 1].getName());
+        System.out.println("-----------------------------");
+        System.out.println("The room alloted is:");
+        System.out.println(r1.generateRoom(HspFile));
 
 
-
- 
         // Labtests
-        var arr = p1.labTestBuilder("itproject/data/LabTests.txt");
+        var arr = p1.labTestBuilder("C:\\Users\\Praveen Raj HL\\Desktop/LabTests.txt");
         var tests = p1.labTestPerformed(arr);
         LabTests[] fine = tests[0];
         LabTests[] needsAttention = tests[1];
